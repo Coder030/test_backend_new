221e90f4f00458bf16cf3d74c87d15deb2ad028b
Coder030 <garg2012030@gmail.com>
2024-02-11 14:58:22 +0530

    cookies are here!

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 // const app = require("./server.js")
 import { app } from "./server";
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 4000
 
 app.listen(PORT, () => {
   console.log('hello on http://localhost:' + PORT);
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,10 @@
 import express from "express"
 import cors from 'cors'
-const cookieParser =  require('cookie-parser')
+// const cookieParser =  require('cookie-parser')
+import cookieParser from 'cookie-parser'
 
 const jobs: string[] = [];
 export const app = express()
-let name2;
 app.use(cors())
 app.use(express.json())
 
@@ -14,17 +14,20 @@ app.get('/', (req, res) => {
   res.json({message: 'this is GET /'})
 })
 
-app.use('/api', () =>{
-  cookieParser()
-})
-app.get('/make_cookie', (req, res) => {
-  name2 = req.body.name
-  res.cookie(name2, 'cookie_value')
+app.use(cookieParser())
+app.post('/make_cookie', (req, res) => {
+  const name2 = req.body.name  
+  return res.cookie("cookie_name", name2).send({ success: true, message: "Success, new user created!"})
 })
 app.post('/get_cookie', (req, res) => {
-  console.log(req.cookies.name2);
-  name2 = req.body.name
-  res.send(req.cookies.name2)
+  const name2 = req.body.name
+  const cookie = req.cookies[name2]
+  if (cookie === undefined){
+    res.json("Sorry, we couldn't find the cookie")
+  }
+  else {
+    res.send(req.cookies[name2])
+  }
 })
 
 app.get('/api/full', (req, res) => {
